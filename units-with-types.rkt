931b45ff28df0f96c711af444e388a2d76c98cd5
Michael Arntzenius <daekharel@gmail.com>
2022-07-12 17:14:25 +0100

    get cartesian closed concategories working, maybe

--- units-with-types.rkt
+++ units-with-types.rkt
@@ -16,40 +16,39 @@
 ;;; ---------- SIGNATURES ----------
 (define-signature cat^
   (nop ;; (A: obj) -> A → A
-   seq ;; ([A, ..., Z]: list obj), A → B, B → C, ..., Y → Z -> A → Z
-   ))
-(define-signature products^
-  (project ;; i, (Aᵢ: obj)ᵢ -> Πᵢⁿ Aᵢ → Aᵢ
-   tuple   ;; Γ, (Aᵢ, fᵢⁿ : Γ → Aᵢ)ᵢ -> Γ → Πᵢ Aᵢ
-   ))
-(define-signature exponentials^
-  (eval      ;; A, B -> (A => B) × A → B
-   transpose ;; Γ, A, (Γ × A → B) -> (Γ → A => B)
-   ))
-(define-signature contexts^
-  (context-empty  ;; Γ
-   context-get    ;; Γ, x:sym -> Γ → A      where x:A ∈ Γ
-   context-extend ;; Γ, x:sym -> (Γ,x:A), (Γ × A → Γ,A)
+   seq ;; ([A, ..., Z]: list obj) -> A → B, B → C, ..., Y → Z -> A → Z
    ))
 
-;; cartesian concategory stuff
-(define-signature cartesian^ extends cat^
+;; cartesian concategory
+(define-signature cartesian-concat^ ; extends cat^
   (;;parallel ;; (Γᵢ, Δᵢ, Γᵢ → Δᵢ)ᵢ -> (Γ₁ ... Γₙ → Δ₁ ... Δₙ)
-   parallel ;; (Γᵢ)ᵢ, (Δᵢ)ᵢ, (Γᵢ → Δᵢ) -> (Γ₁ ... Γₙ → Δ₁ ... Δₙ)
    ;;permute  ;; Γ, π -> π(Γ) → Γ
-   fork ;; Γ, (Δᵢ)ᵢ, (Γ → Δᵢ)ᵢ -> Γ → (Δᵢ)ᵢ
-   rename ;; Γ, ρ -> ρ(Γ) → Γ
-   merge ;; (Aᵢ)ᵢ -> A₁, ..., Aₙ → A₁ ⊗ ... ⊗ Aₙ
-   split ;; (Aᵢ)ᵢ -> A₁ ⊗ ... ⊗ Aₙ → A₁, ..., Aₙ
-   (define-values (project) (lambda (Γ . idxs) (rename Γ idxs)))
+   parallel ;; (Γᵢ)ᵢ, (Δᵢ)ᵢ -> (Γᵢ → Δᵢ)ᵢ -> (Γ₁ ... Γₙ → Δ₁ ... Δₙ)
+   fork     ;; Γ, (Δᵢ)ᵢ -> (Γ → Δᵢ)ᵢ -> Γ → (Δᵢ)ᵢ
+   ;; TODO: fork can be derived from rename & para, omit or define it?
+   rename   ;; Γ, ρ -> ρ(Γ) → Γ
+   merge    ;; (Aᵢ)ᵢ -> A₁, ..., Aₙ → A₁ ⊗ ... ⊗ Aₙ
+   split    ;; (Aᵢ)ᵢ -> A₁ ⊗ ... ⊗ Aₙ → A₁, ..., Aₙ
+   (define-values (select) (lambda (Γ . idxs) (rename Γ idxs)))
    ))
 
+;; ;; closed concategory, v1
+;; (define-signature closed-concat^
+;;   (eval      ;; Γ, Δ    -> (Γ => Δ), Γ → Δ
+;;    transpose ;; Γ, Δ, Ω -> (Γ,Δ → Ω) -> Γ → (Δ => Ω)
+;;    ))
+
+;; closed concategory, v2 (thanks Jacques Carette for the suggestion)
+(define-signature closed-concat^
+  (uncurry ;; Γ, Δ, Ω  ->  Γ → (Δ => Ω)  ->  Γ,Δ → Ω
+   curry   ;; Γ, Δ, Ω  ->  (Γ,Δ → Ω)     ->  Γ → (Δ => Ω)
+   ))
 
 
 #;
 (define-unit racket-multi@
   (import)
-  (export cartesian^)
+  (export cartesian-concat^)
   (define (nop As) values)
   (define (seq As . funcs) (apply compose (reverse funcs)))
   (define ((parallel tagged-funcs) . inputs)
@@ -62,22 +61,43 @@
 ;; TODO: shouldn't we be putting type info here?
 (define dataflow-nodes (make-parameter '()))
 (define/contract (dataflow-node! morphism ins outs)
-  (-> (or/c symbol? syntax?) (listof symbol?) (listof symbol?) void?)
-  (dataflow-nodes (cons (list morphism ins outs) (dataflow-nodes))))
+  ;; TODO: probably don't want any/c here
+  (-> (or/c symbol? syntax? any/c) (listof symbol?) (listof symbol?) (listof symbol?))
+  (dataflow-nodes (cons (list morphism ins outs) (dataflow-nodes)))
+  outs)
 (define (dataflow-apply! function ins)
   (define out (gensym (cond [(symbol? function) function]
                             [(identifier? function) (syntax-e function)]
                             [#t "tmp"])))
-  (dataflow-node! function ins `(,out))
-  (list out))
+  (dataflow-node! function ins `(,out)))
 (define (dataflow-call! function . ins) (dataflow-apply! function ins))
 
+(define-signature string-extras^ (string->racket-body string->racket))
 (define-unit strings@
   (import)
-  (export cartesian^)
+  (export cat^ cartesian-concat^ closed-concat^ string-extras^)
+  (define (string->racket-body morph inputs)
+    (define-values (outs nodes)
+      (parameterize ([dataflow-nodes '()])
+        (define outs (morph inputs))
+        (values outs (reverse (dataflow-nodes)))))
+    `(,@(for/list ([n nodes])
+          (match-define (list func ins outs) n)
+          (match outs
+            ['() `(,func ,@ins)]
+            [`(,x) `(define ,x (,func ,@ins))]
+            [xs `(define-values ,xs (,func ,@ins))]))
+      ,(match outs
+         ['() '(void)]
+         [`(,x) x]
+         [outs `(values ,@outs)])))
+  (define (string->racket morph inputs)
+    `(let () ,@(string->racket-body morph)))
+
+  ;; -- concategory --
   (define (nop A) identity)
-  (define (seq As . funcs) (apply compose1 (reverse funcs)))
-  (define ((parallel Γs Δs fs) inputs)
+  (define ((seq . As) . funcs) (apply compose1 (reverse funcs)))
+  (define (((parallel Γs Δs) fs) inputs)
     ;; #:do not available until Racket 8.4
     #;(for/list ([Γ Γs] [Δ Δs] [f fs]
                #:do [(define-values (before after) (split-at inputs (length Γ)))
@@ -91,26 +111,42 @@
        (define outs (f before))
        (unless (length=? Δ outs) (error "wrong # of outputs"))
        outs)))
-  ;; Γ, (Δᵢ)ᵢ, (Γ → Δᵢ)ᵢ -> Γ → (Δᵢ)ᵢ
-  (define ((fork Γ Δs fs) inputs)
+
+  ;; -- cartesian --
+  ;; Γ, (Δᵢ)ᵢ -> (Γ → Δᵢ)ᵢ -> Γ → (Δᵢ)ᵢ
+  (define (((fork Γ Δs) fs) inputs)
     (for*/list ([f fs] [x (f inputs)]) x))
   (define ((rename Γ rho) inputs)
     (for/list ([i rho]) (list-ref inputs i)))
   (define ((merge As) inputs)
     (unless (= (length As) (length inputs)) (error "wrong # of inputs"))
-    (dataflow-apply! #'list inputs))
+    (dataflow-apply! 'list inputs))
   (define ((split As) inputs)
     (unless (= 1 (length inputs)) (error "too many inputs"))
-    (dataflow-apply! #'list->values inputs)))
+    (dataflow-node! 'list->values inputs (for/list ([_ As]) (gensym 'list->values))))
+
+  ;; -- closed --
+  (define (((uncurry Γ Δ Ω) morph) inputs)
+    (define-values (gamma delta) (split-at inputs (length Γ)))
+    (match-define `(,f) (morph gamma))
+    (dataflow-apply! 'apply (cons f delta)))
+  (define (((curry Γ Δ Ω) morph) gamma)
+    ;; TODO: this feels kinda hackish. it also generates slightly ugly code.
+    ;; maybe the right way to do this is in Dan Ghica's work somewhere.
+    (define delta (for/list ([_ Δ]) (gensym 'arg)))
+    (dataflow-node!
+     `(lambda ,gamma (lambda ,delta ,@(string->racket-body morph (append gamma delta))))
+     gamma
+     (for/list ([_ Ω]) (gensym 'result)))))
 
 
 ;; ---------- STLC ----------
 (define-signature stlc^
-  (var       ;; x -> (Γ → A)      where x:A ∈ Γ
-   lam       ;; x, (Γ,x:A → B) -> (Γ → A => B)
-   app       ;; Γ, A, B, (Γ → A => B), (Γ → A) -> (Γ → B)
-   tuple     ;; Γ, (Aᵢ)ᵢ, (Γ → Aᵢ)ᵢ -> (Γ → Πᵢⁿ Aᵢ)
-   let-tuple ;; Γ, (xᵢ)ᵢ, (Aᵢ)ᵢ, B, (Γ → Πᵢ Aᵢ), (Γ,(xᵢ:Aᵢ)ᵢ → B) -> Γ → B
+  (var       ;; Γ, x, A    ->  Γ → A      where x:A ∈ Γ
+   lam       ;; Γ, x, A, B ->  Γ,x:A → B          ->  Γ → A => B
+   app       ;; Γ, A, B    ->  Γ → A => B, Γ → A  ->  Γ → B
+   tuple     ;; Γ, Aᵢ...   ->  (Γ → Aᵢ)...        ->  Γ → Πᵢⁿ Aᵢ
+   let-tuple ;; Γ, (xᵢ)ᵢ, (Aᵢ)ᵢ, B  ->  Γ → Πᵢ Aᵢ, Γ,(xᵢ:Aᵢ)ᵢ → B -> Γ → B
    ))
 
 (define stlc-context (make-parameter (hash)))
@@ -120,24 +156,40 @@
 (define-syntax-rule (stlc-with xs body ...)
   (parameterize ([stlc-context (stlc-extend xs)]) body ...))
 
+;; uncurry ;; Γ, Δ, Ω  ->  Γ → (Δ => Ω)  ->  Γ,Δ → Ω
+;; curry   ;; Γ, Δ, Ω  ->  (Γ,Δ → Ω)     ->  Γ → (Δ => Ω)
+
 (define-unit concat->stlc@
-  (import cartesian^)
+  (import cat^ cartesian-concat^ closed-concat^)
   (export stlc^)
-  (define ((var Γ name))
-    (project Γ (hash-ref (stlc-context) name)))
-  (define ((lam Γ A e)) TODO)
-  (define ((app Γ A B e1 e2)) TODO)
-  (define ((tuple Γ As es))
-    (seq
-     (list Γ As `((tuple ,@As)))
-     (fork Γ (map list As) (map (lambda (x) (x)) es))
+  (define ((var Γ x A))
+    (select Γ (hash-ref (stlc-context) x)))
+  (define (((lam Γ x A B) e))
+    ;; (stlc-with '(x) (e)): Γ,A → B
+    ;; want: Γ → (A) => (B)
+    ((curry Γ `(,A) `(,B)) (stlc-with '(x) (e))))
+  (define (((app Γ A B) e1 e2))
+    ;; e1               Γ → A => B
+    ;; e2               Γ → A
+    ;; fork (id, e2)    Γ → Γ,A         <- we compose
+    ;; uncurry e1       Γ,A → B         <- these two
+    ;; desired          Γ → B
+    ((seq Γ (append Γ `(,A)) `(,B))
+     ((fork Γ `(,Γ (,A)))
+      (list (nop Γ) (e2)))
+     ((uncurry Γ `(,A) `(,B))
+      (e1))))
+  (define (((tuple Γ . As) . es))
+    ((seq Γ As `((tuple ,@As)))
+     ((fork Γ (map list As))
+      (map (lambda (x) (x)) es))
      (merge As)))
-  (define ((let-tuple Γ xs As B expr body))
-    (seq
-     `(,Γ ,(append Γ As) (,B))
+  (define (((let-tuple Γ xs As B) expr body))
+    ((seq Γ (append Γ As) `(,B))
      ;; fork id (expr;split)
-     (fork Γ (list Γ As)
-           (list nop (seq `(,Γ ((tuple ,@As)) ,As) (expr) (split As))))
+     ((fork Γ (list Γ As))
+      (list nop ((seq `(,Γ ((tuple ,@As)) ,As))
+                 (expr) (split As))))
      (stlc-with xs (body)))))
 
 (define-compound-unit/infer stlc-strings@
@@ -153,11 +205,32 @@
     (define string-maker
       (parameterize ([stlc-context (stlc-extend xs (hash))])
         (term)))
-    (parameterize ([dataflow-nodes '()])
-      (define outs (string-maker xs))
-      (values outs (dataflow-nodes))))
+    (define-values (outs nodes)
+     (parameterize ([dataflow-nodes '()])
+       (define outs (string-maker xs))
+       (values outs (reverse (dataflow-nodes)))))
+    ;; a very simple compilation into Racket code.
+    `(let ()
+       ,@(for/list ([n nodes])
+           (match-define (list func ins outs) n)
+           (match outs
+             ['() `(,func ,@ins)]
+             [`(,x) `(define ,x (,func ,@ins))]
+             [xs `(define-values ,xs (,func ,@ins))]))
+       ,(match outs
+          ['() '(void)]
+          [`(,x) x]
+          [outs `(values ,@outs)])))
 
   (define Γ '(A B))
-  (run '(a b) Γ '((tuple B A))
-       (tuple Γ '(B A)
-              (list (var Γ 'b) (var Γ 'a))))  )
+  (pretty-print
+   (run '(a b) Γ '((tuple B A))
+        ((tuple Γ 'B 'A)
+         (var Γ 'b 'B)
+         (var Γ 'a 'A))))
+
+  (pretty-print
+   (run '(a) '(A) '((-> (B) (A)))
+        ((lam '(A) 'b 'B 'A)
+         (var '(A B) 'a 'A))))
+  )
